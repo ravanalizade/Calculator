e5928bb2e0a5041ca5bca612317078d81d16541b
AlizadeRavan <alizzaderavan@gmail.com>
2024-10-18 23:36:14 +0300

    text

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,9 +2,46 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Main {
+public class Calculator implements ActionListener {
+
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subtractButton, multiplyButton, divideButton;
+    JButton decimalButton, equButton, delButton, clrButton;
+
+    JPanel jp;
+
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
+
+    Calculator() {
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
+
+        frame.add(textField);
+        frame.setVisible(true);
+
+    }
+
+
     public static void main(String[] args) {
 
-        
+        Calculator calculator = new Calculator();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
